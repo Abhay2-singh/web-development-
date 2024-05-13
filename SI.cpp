4629428275ef36a462a2d2147b27c10d14209681
Abhay singh rajawat <166044966+Abhay2-singh@users.noreply.github.com>
2024-05-13 10:30:04 +0530

    Add files via upload

---
+++ SI.cpp
@@ -0,0 +1,15 @@
+//calculate si
+#include<iostream>
+using namespace std;
+int main()
+{
+    int p,t,si;
+    float r;
+    cout<<"\nenter the no";
+    cin>>p>>r>>t;
+    si=p*r*t/100;
+    cout<<"\nsimple interest="<<si;
+    return 0;2
+    
+
+}
