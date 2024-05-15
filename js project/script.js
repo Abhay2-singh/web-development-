4d2ca968985a9e5b7883c578861122cb05cd5ffe
Abhay singh rajawat <166044966+Abhay2-singh@users.noreply.github.com>
2024-05-15 08:57:26 +0530

    Add files via upload

---
+++ js project/script.js
@@ -0,0 +1,8 @@
+let btn=document.getElementById('togglebtn')
+let bulb=document.getElementById('bulb')
+btn.addEventListener('click',toggleBulb)
+
+function toggleBulb(e)
+{
+   bulb.src="https://media.istockphoto.com/id/1188274736/photo/burning-classical-lightbulb-levitating-in-the-air.webp?b=1&s=170667a&w=0&k=20&c=KerQ33RO77qWBa7m4lz0e2jVrIRkL53lcd4Ra2_cACg="
+}
